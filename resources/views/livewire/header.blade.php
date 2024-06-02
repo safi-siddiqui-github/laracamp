67eaef8dd6b83224601e077124aeac5dc12b2761
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-06-03 00:42:33 +0500

    asset-vite-completed

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -16,7 +16,6 @@ use function Livewire\Volt\{state};
         <a href="{{route('logout')}}" class="rounded-full border pl-2 pr-4 py-1 flex items-center gap-1 hover:border-black">
             <img src="{{auth()->user()->avatar}}" alt="avatar" class="w-10 h-10 rounded-full">
             <span class="font-semibold">{{auth()->user()->name}}</span>
-            <!-- <span>Sign Out</span> -->
             <span>Logout</span>
         </a>
         @else
