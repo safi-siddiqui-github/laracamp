0437007f68c7a6e96fa0dca6410cafcff6bc89f2
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-06-03 00:34:02 +0500

    asset-vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     plugins: [
@@ -8,10 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        basicSsl(),
     ],
-    server: {
-        origin: 'https://laracamp.fly.dev',
-        host: 'laracamp'
-      },
 });
