4ecf2f4cada9926ea4c1f7bbf382dc4ff924dd5a
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-06-29 23:59:22 +0500

    'update'

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import '../css/app.css'
+import './ziggy.js'
 
 createInertiaApp({
   // Below you can see that we are going to get all React components from resources/js/Pages folder
